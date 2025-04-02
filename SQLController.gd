b4cc21b1297053b9c11a6e8ecc08b236244b8248
Evan <evan.c.kelch@gmail.com>
2025-04-01 23:11:25 -0400

    songs loading into database and play button working

--- SQLController.gd
+++ SQLController.gd
@@ -3,6 +3,8 @@ extends Control
 var database: SQLite
 @onready var field_songName: TextEdit = $NameContainer/TextEdit
 @onready var field_artistName: TextEdit = $ArtistContainer/TextEdit
+@onready var song_list: VBoxContainer = $SongList
+@onready var audio_stream_player: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 func _ready():
 	database = SQLite.new()
@@ -10,38 +12,65 @@ func _ready():
 	database.open_db()
 
 func create_database() -> void:
-	var assets = DirAccess.open("res://assets")
-
-	if assets:
-		assets.list_dir_begin()
-		var file_name = assets.get_next()
+	var songs = {
+		"id": {"data_type": "int", "primary_key": true, "auto_increment": true, "not_null": true},
+		"file_name": {"data_type": "text", "unique": true},
+		"song_name": {"data_type": "text"},
+		"artist_id": {"data_type": "int"},
+		"genre_id": {"data_type": "int"}
+	}
+	database.create_table("songs", songs)
+	
+func populate_database_from_assets():
+	var dir = DirAccess.open("res://assets")
+	if dir:
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
 		while file_name != "":
-			if assets.current_is_dir():
-				print("Dir found: " + file_name)
-			else:
-				print("file found: " + file_name)
-			assets.get_next()
+			if !dir.current_is_dir() && file_name.ends_with(".mp3"):
+				var data = {
+					"file_name": file_name,
+					"song_name": file_name,
+					"artist_id": null,
+					"genre_id": null
+				}
+				database.query(insertSongQuery(file_name))
+			file_name = dir.get_next()
 	else:
 		print("Failed to open directory")
-	#var songs = {
-		#"id": {"data_type": "int", "primary_key": true, "auto_increment": true, "not_null": true},
-		#"song_name": {"data_type": "text"},
-		#"artist_name": {"data_type": "text"}
-	#}
-	#database.create_table("songs", songs)
+
+func insertSongQuery(file_name: String) -> String:
+	var fstr = "INSERT OR IGNORE INTO songs(file_name, song_name) VALUES('%s','%s')"
+	return fstr % [file_name, file_name]
 
 func _on_insert_data_pressed() -> void:
-	if field_songName.text != "" && field_artistName.text != "":
-		var data = {
-			"song_name": field_songName.text,
-			"artist_name": field_artistName.text
-		}
-		database.insert_row("songs", data)
-
+	create_database()
+	populate_database_from_assets()
 
 func _on_select_data_pressed() -> void:
-	print(database.select_rows("songs", "", ["*"]))
+	var query_result = database.select_rows("songs", "", ["*"])
+	
+	for child in song_list.get_children(false):
+		song_list.remove_child(child)
+		
+	for song in query_result:
+		var hbox = HBoxContainer.new()
+		var button = Button.new()
+		button.text = "play"
+		print(song)
+		print(song.file_name)
+		button.pressed.connect(play_song.bind(song.file_name))
+		var label = Label.new()
+		label.text = song.song_name
+		hbox.add_child(button)
+		hbox.add_child(label)
+		song_list.add_child(hbox)
 
+func play_song(file_name: String):
+	audio_stream_player.stop()
+	var audio_stream = AudioStreamMP3.load_from_file("res://assets/" + file_name)
+	audio_stream_player.stream = audio_stream
+	audio_stream_player.play()
 
 func _on_delete_data_pressed() -> void:
-	create_database()
+	pass
